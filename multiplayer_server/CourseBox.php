ca612dac844380f80a17f2486f908ed397cb49e9
fourst4r <oxygenmagnet@hotmail.com>
2025-10-19 18:41:51 +1300

    remove another rank check
    bro was scarred

--- multiplayer_server/CourseBox.php
+++ multiplayer_server/CourseBox.php
@@ -168,18 +168,10 @@ class CourseBox
         $course_id = substr($this->course_id, 0, strpos($this->course_id, '_'));
         $game = new Game($course_id, $this->room->getType());
         foreach ($this->slot_array as $player) {
-            if ($player->active_rank < 100 || $player->user_id === FRED) {
-                $player->confirmed = false;
-                $game->addPlayer($player);
-                client_set_right_room($player->socket, 'none');
-                client_set_chat_room($player->socket, 'none');
-            } else {
-                $slot = $player->slot; // get slot
-                $player->write('message`Some data was incorrect. Please log in again.');
-                $player->remove(); // disconnect them
-                $this->slot_array[$slot] = null; // clear that slot
-                unset($this->slot_array[$slot]); // ^
-            }
+            $player->confirmed = false;
+            $game->addPlayer($player);
+            client_set_right_room($player->socket, 'none');
+            client_set_chat_room($player->socket, 'none');
         }
         $game->init();
         $this->remove(false, true);
