9adf6a8d76edb4d10156812373eb4c1b18898823
fourst4r <oxygenmagnet@hotmail.com>
2025-10-22 15:57:06 +1300

    Update upload_level.php

--- http_server/upload_level.php
+++ http_server/upload_level.php
@@ -269,12 +269,10 @@ try {
 
     // write to the file system
     $file_path = WWW_ROOT . "/levels/$level_id.txt";
-    $was_writable = false;
     $file = fopen($file_path, "w");
     if ($file !== false) {
         fwrite($file, $str);
         fclose($file);
-        $was_writable = true;
     }
 
     // save the new file to the backup system
@@ -313,7 +311,7 @@ try {
             'your level was not added to the newest levels list. If you wish to have your level on newest, '.
             'wait for your other levels to disappear off page 1 of newest and then publish again.';
     } else {
-        echo 'message=The save was successful.' . 'writeable=' . ($was_writable ? '1' : '0') . ', path=' . $file_path;
+        echo 'message=The save was successful.';
     }
 } catch (Exception $e) {
     $error = $e->getMessage();
